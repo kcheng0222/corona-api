138b7bbb90ae460431bb3927e096b42390bfb6df
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2020-04-17 15:29:40 -0700

    pretty print function and get countries/summary

--- corona.py
+++ corona.py
@@ -1,7 +1,16 @@
 import requests
 import json
 
+def pp(d):
+    print(json.dumps(d, indent=2))
+
 api_url = "https://api.covid19api.com/"
 response = requests.get(api_url)
 r = response.json()
-print(json.dumps(r, indent=2))
+#pp(r)
+
+allCountries = requests.get(api_url + "countries").json()
+#pp(allCountries)
+
+summary = requests.get(api_url + "summary").json()
+pp(summary)
