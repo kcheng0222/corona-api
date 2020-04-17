0ed3c434158200abeafaaa6222d2699c15db138c
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2020-04-17 16:34:45 -0700

    Replaced individual comment with one big comment

--- corona.py
+++ corona.py
@@ -5,16 +5,17 @@ def pp(d):
     print(json.dumps(d, indent=2))
 
 api_url = "https://api.covid19api.com/"
-#response = requests.get(api_url)
-#r = response.json()
-#pp(r)
-
-#allCountries = requests.get(api_url + "countries").json()
-#pp(allCountries)
-
-#summary = requests.get(api_url + "summary").json()
-#pp(summary)
 '''
+response = requests.get(api_url)
+r = response.json()
+pp(r)
+
+allCountries = requests.get(api_url + "countries").json()
+pp(allCountries)
+
+summary = requests.get(api_url + "summary").json()
+pp(summary)
+
 for x in summary["Countries"]:
     if x["CountryCode"] == "US":
         #print(x)
